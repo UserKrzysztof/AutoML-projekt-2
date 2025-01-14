1478deec6ca66be388b6c01f6dd7a986ca320cf2
nataliachoszczyk <126119086+nataliachoszczyk@users.noreply.github.com>
2025-01-14 11:18:50 +0100

    update tutorial

--- autopocket/main.py
+++ autopocket/main.py
@@ -15,6 +15,9 @@ class AutoPocketor():
     def doJob(self,
               path,
               target,
+              num_strategy='mean', 
+              cat_strategy='most_frequent', 
+              fill_value=None,
               generate_shap=True,
               generate_lime_pdp_ice=True,
               features_for_displaying_plots=None,
@@ -29,6 +32,12 @@ class AutoPocketor():
                 Path to the dataset file.
             target: str
                 Name of the target column in the dataset.
+            num_strategy: string, optional, default='mean'
+                The imputation strategy for numerical columns ('mean', 'median', 'most_frequent', 'constant').
+            cat_strategy: string, optional, default='most_frequent'
+                The imputation strategy for categorical columns ('most_frequent', 'constant').
+            fill_value: Any, optional, default=None
+                The value to use when filling missing values, if 'constant' strategy is used.
             generate_shap: bool, optional, default=True
                 Whether to generate SHAP explanations in postprocessing.
             generate_lime_pdp_ice: bool, optional, default=True
@@ -61,17 +70,30 @@ class AutoPocketor():
         """
 
         print("Performing preprocessing...")
-        X, y, ml_type = Preprocessor().preprocess(path = path, target=target)
+        X, y, ml_type = Preprocessor().preprocess(path = path, 
+                                                  target=target, 
+                                                  num_strategy=num_strategy, 
+                                                  cat_strategy=cat_strategy, 
+                                                  fill_value=fill_value)
         print("X shape:", X.shape)
         print("Preprocessing done.")
 
         print("\nPerforming modelling...")
-        best_model, results_dir = Modeller(additional_estimators=additional_estimators).model(X, y, ml_type) ####
+        best_model, results_dir = Modeller(additional_estimators=additional_estimators).model(X, 
+                                                                                              y, 
+                                                                                              ml_type)
         print(f"Chosen model: {best_model.__class__.__name__}")
         print("Modelling done.")
 
         X = pd.DataFrame(X)
         
         print("\nPerforming postprocessing...")
-        Postprocessor(results_dir=results_dir).postprocess(best_model, X, y, ml_type, generate_shap, generate_lime_pdp_ice, features_for_displaying_plots, subset_fraction_for_ICE_plot)
+        Postprocessor(results_dir=results_dir).postprocess(best_model, 
+                                                           X, 
+                                                           y, 
+                                                           ml_type, 
+                                                           generate_shap, 
+                                                           generate_lime_pdp_ice, 
+                                                           features_for_displaying_plots, 
+                                                           subset_fraction_for_ICE_plot)
         print("Postprocessing done.")
